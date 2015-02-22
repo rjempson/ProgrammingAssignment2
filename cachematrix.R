c57be71f5fde346f7f7b92c858f8f7c58934ea17
Ross Jempson <rjempson@gmail.com>
2015-02-22 19:31:45 +1000

    Implemented functions as requested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Use makeCachedMatrix to make an object that can store and return a matrix and it's inverse.
+##
+## An object created by makeCachedMatrix can be passed to cacheSolve which will return the inverse of the matrix,
+## but makeCachedMatrix will only calculate the inverse the first time it is called and will use a cached copy
+## of the inverse on subsequent calls.
 
-## Write a short comment describing this function
+## Usage :
+## x <- matrix(1:4, 2, 2)   # Create a 2x2 matrix
+## cm <- makeCacheMatrix(x) # Create the wrapper object
+## invX <- cacheSolve(cm)   # on this occasion the inverse will be calculated and returned
+## invX2 <- cacheSolve(cm)  # on this occasion a cached copy of the inverse will be returned
 
+## makeCacheMatrix takes a square matrix as a parameter (or defaults to an empty matrix)
+## Returns a list of functions over x that can be used to set/get the matrix and it's inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve takes an object created by makeCacheMatrix
+## It will return the current stored value of the inverse if it is not null
+## If the inverse is null, the function calculates the inverse, stores it, then returns it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+      message("getting cached inverse")
+      return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
